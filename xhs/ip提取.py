710cdd1086e2a17a7a7967406e7c3932c6fcae1b
AliDBD <89066931+AliDBD@users.noreply.github.com>
2023-12-13 13:56:55 +0800

    更新解析文件

--- xhs/ip提取.py
+++ xhs/ip提取.py
@@ -9,12 +9,13 @@
 # @File : ip提取.py
 import time
 
+import pandas as pd
 import requests
 
 
 import requests
 
-def poxie():
+# def poxie():
     # 代理信息
     # proxy_ip = "d842.kdltps.com"
     # proxy_port = 15818
@@ -35,19 +36,29 @@ def poxie():
     # except Exception as e:
     #     print(f"发生错误: {e}")
 
+ip_data = []
+for i in range(5):
+    time.sleep(
+        2
+    )
     url = 'https://ip.cn/api/index?ip=&type=0'
     username = 't17037773479161'
     password = 'lhlnpdmj'
     proxy = f"http://{username}:{password}@d842.kdltps.com:15818"
-    result = requests.get(url, proxies={
-        'http': proxy,
-        'https': proxy
-    })
+    try:
+        result = requests.get(url, proxies={
+            'http': proxy,
+            'https': proxy
+        })
+        ip = result.json()['ip']
+        address = result.json()['address']
+        print(f"\nIP归属地：{address},IP地址：{ip}")
+        ip_data.append({"归属地":address,"IP":ip})
+    except (requests.RequestException,Exception) as e:
+        print("请求失败！")
 
-    ip = result.json()['ip']
-    print(ip)
+df = pd.DataFrame(ip_data)
+df.to_excel(r'E:\2023年\spider\temp_ip.xlsx', index=False)
+print("ip保存本地完成！")
 
 
-for i in range(2):
-    time.sleep(1)
-    poxie()
