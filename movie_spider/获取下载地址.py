8dd0c0496e3288a71597904168f199a0a6ee7183
AliDBD <89066931+AliDBD@users.noreply.github.com>
2023-12-13 16:00:19 +0800

    修改了配置

--- movie_spider/获取下载地址.py
+++ movie_spider/获取下载地址.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-
 # -- coding: utf-8 --
-
 # @Time : 2023/12/12 17:12
-
 # @Author : Genesis Ai
-
 # @File : 获取下载地址.py
+'''
+代码仅提供逻辑，相似场景和应用基于此逻辑方法可以实现，具体内容和数据需求需要适当调整解析方式
+本代码主要是根据需求获取某站点的可下载url链接（mp4格式）储存到本地
+'''
 
 import time
 import requests
